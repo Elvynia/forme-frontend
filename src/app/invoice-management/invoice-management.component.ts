b50da552c304a1335769387d109a5ce4ddd5cc67
elvynia <elvynia@gmail.com>
2018-05-21 00:11:45 +0200

    Added route query param invoiceId handled in InvoiceManagement component as pre-selection.

--- src/app/invoice-management/invoice-management.component.ts
+++ src/app/invoice-management/invoice-management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Invoice } from '../invoice';
 import { InvoiceService } from '../invoice.service';
 import { EVENT } from '../event';
@@ -11,9 +12,14 @@ import { EVENT } from '../event';
 export class InvoiceManagementComponent implements OnInit {
     selectedId: number;
 
-    constructor(private invoiceService: InvoiceService) { }
+    constructor(private invoiceService: InvoiceService, private route: ActivatedRoute) { }
 
     ngOnInit() {
+        this.route.queryParamMap.subscribe((params) => {
+            if (params.has('invoiceId')) {
+                this.selectedId = parseInt(params.get('invoiceId'));
+            }
+        });
         this.invoiceService.eventsByType(EVENT.DELETE)
             .filter((invoice) => this.selectedId && invoice.id === this.selectedId)
             .subscribe(() => this.selectedId = undefined);
